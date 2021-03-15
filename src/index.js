8e06e85b0b2822ce6dec25578d6f86fd24d189ce
Ben Mandeberg <bmandeberg@ucla.edu>
2021-03-14 17:28:59 -0700

    Boilerplate app initialization

---
+++ src/index.js
@@ -0,0 +1,5 @@
+if (process.env.NODE_ENV === 'development') {
+  require('../dist/index.html')
+}
+
+console.log('Hello Project.')
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['raw-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'build.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
